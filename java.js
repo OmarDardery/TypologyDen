d623774b34ffded3d57809e136cbb2cab7064ce7
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2024-07-26 02:25:04 +0300

    Update java.js

--- java.js
+++ java.js
@@ -4,18 +4,19 @@ menuButton.addEventListener("click", function(){
     menuButton.classList.toggle("rotated");
     menu.classList.toggle("hidden-menu");
     if(document.querySelector(".opacity").classList.contains("hidden")){
-        document.querySelector(".opacity").classList.toggle("hidden");
-        document.querySelector(".opacity").classList.toggle("opacityy");
+        document.querySelector(".opacity").classList.remove("hidden");
+        void document.querySelector(".opacity").offsetWidth;
+
+        document.querySelector(".opacity").classList.add("opacityy");
 
     }else{
         
-        document.querySelector(".opacity").classList.toggle("opacityy");
+        document.querySelector(".opacity").classList.remove("opacityy");
         document.querySelector(".opacity").addEventListener("transitionend", function(){
-            document.querySelector(".opacity").classList.toggle("hidden");
-        });
+            document.querySelector(".opacity").classList.add("hidden");
+        }, { once: true });
         
     }
-
 });
 
 var MBTIContainer = document.querySelector(".mbti");
