ca95184bbfcec1d738e04dd8131f8f34c892adaa
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2024-07-25 21:40:00 +0300

    transitions complete

--- java.js
+++ java.js
@@ -2,6 +2,6 @@ var menuButton = document.querySelector(".menubutton");
 var menu = document.querySelector(".menu");
 menuButton.addEventListener("click", function(){
     menuButton.classList.toggle("rotated");
-    menu.classList.toggle("transitionEnd");
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("hidden-menu");
+    document.querySelector(".opacity").classList.toggle("hidden");
 });
