bd592b36d2ee94b2db82b05d0e7534ac66257322
Omar Dardery <omar71creatorcodefortnite@gmail.com>
2024-07-26 15:26:57 +0300

    jquery incorporated

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-var menuButton = document.querySelector(".menubutton");
-var menu = document.querySelector(".menu");
-menuButton.addEventListener("click", function(){
-    menuButton.classList.toggle("rotated");
-    menu.classList.toggle("hidden-menu");
-    if(document.querySelector(".opacity").classList.contains("hidden")){
-        document.querySelector(".opacity").classList.remove("hidden");
-        void document.querySelector(".opacity").offsetWidth;
+var menuButton = $(".menubutton");
+var menu = $(".menu");
+var opacity = $(".opacity");
 
-        document.querySelector(".opacity").classList.add("opacityy");
-        document.querySelector(".opacity").addEventListener("click", function(){
-            document.querySelector(".opacity").classList.remove("opacityy");
-            document.querySelector(".opacity").addEventListener("transitionend", function(){
-                document.querySelector(".opacity").classList.add("hidden");
-            }, { once: true });
-            menuButton.classList.toggle("rotated");
-            menu.classList.toggle("hidden-menu");
+menuButton.on("click", function() {
+    menuButton.toggleClass("rotated");
+    menu.toggleClass("hidden-menu");
 
-        }, { once: true });
-    }else{
-        
-        document.querySelector(".opacity").classList.remove("opacityy");
-        document.querySelector(".opacity").addEventListener("transitionend", function(){
-            document.querySelector(".opacity").classList.add("hidden");
-        }, { once: true });
-        document.querySelector(".opacity").addEventListener("click", function(){
-            document.querySelector(".opacity").classList.remove("opacityy");
-            document.querySelector(".opacity").addEventListener("transitionend", function(){
-                document.querySelector(".opacity").classList.add("hidden");
-            }, { once: true });
-            menuButton.classList.toggle("rotated");
-            menu.classList.toggle("hidden-menu");
-            
-        }, { once: true });
-        
+    if (opacity.hasClass("hidden")) {
+        opacity.removeClass("hidden");
+        void opacity[0].offsetWidth; // Trigger a reflow
+
+        opacity.addClass("opacityy");
+        opacity.one("click", function() {
+            opacity.removeClass("opacityy");
+            opacity.one("transitionend", function() {
+                opacity.addClass("hidden");
+            });
+            menuButton.toggleClass("rotated");
+            menu.toggleClass("hidden-menu");
+        });
+    } else {
+        opacity.removeClass("opacityy");
+        opacity.one("transitionend", function() {
+            opacity.addClass("hidden");
+        });
+        opacity.one("click", function() {
+            opacity.removeClass("opacityy");
+            opacity.one("transitionend", function() {
+                opacity.addClass("hidden");
+            });
+            menuButton.toggleClass("rotated");
+            menu.toggleClass("hidden-menu");
+        });
     }
 });
-
-var MBTIContainer = document.querySelector(".mbti");
-MBTIContainer.addEventListener("click", function(){
-    window.location.href = "https://www.google.com";
-});
+// the part above is for the menu functionality
+// the part below is for the rest
